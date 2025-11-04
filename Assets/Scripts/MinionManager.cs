0343b9223aec6d30b8ec2715dba24bd9e0dc7e19
daruwu <dariomcamino@gmail.com>
2025-11-04 00:31:30 -0300

    add minionmanager

---
+++ Assets/Scripts/MinionManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MinionManager : MonoBehaviour
+{
+    float spawnTimer = 30f;
+    float nextSpawn = 0f;
+    void Start()
+    {
+      
+    }
+    void Update()
+    {  
+    }
+    void HandleMinionSpawn()
+    {
+
+    }
+}
